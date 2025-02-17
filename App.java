fefe6582cd0ca4adad3c76a5c4fc722400e6e83a
Rian <rianmagp573@gmail.com>
2025-02-17 13:03:25 -0300

    Novas Classes View

--- App.java
+++ App.java
@@ -2,33 +2,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class App extends Application{
-    public static void main(String[] args) throws Exception {
+public class App extends Application {
+    public static void main(String[] args) {
         launch(args);
     }
 
-    public void start(Stage primaryStage) throws Exception{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layout.fxml"));
         Parent root = fxmlLoader.load();
-        Scene tela = new Scene(root);
+        Scene scene = new Scene(root);
 
         primaryStage.setTitle("POO das Canoas");
-        primaryStage.setScene(tela);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    public class EclusaView {
-    public static void atualizarBotao(Button button, boolean aberto) {
-        button.setStyle(aberto ? "-fx-background-color: green;" : "-fx-background-color: red;");
-    }
-
-    public static void atualizarNivelAgua(ImageView agua, double nivel) {
-        agua.setLayoutY(nivel);
-    }
 }
-}
-
-
