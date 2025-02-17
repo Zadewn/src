bb25a8daaadec14197aa1b4872d147f7c384f251
Rian <rianmagp573@gmail.com>
2025-02-17 15:37:10 -0300

    Linhas

--- Container.java
+++ Container.java
@@ -1,34 +1,13 @@
-
 public class Container {
-
     private float peso;
-    private String empresa;
 
-    public float getPeso() {
-        return peso;
-    }
+    public float getPeso() { return peso; }
 
-    public String getEmpresa() {
-        return empresa;
-    }
+    public void setPeso(float peso) { if (peso > 0) this.peso = peso; }
 
-    public void setPeso(float peso) {
-        if (peso > 0) {
-            this.peso = peso;
-        }
-    }
-
-    public void setEmpresa(String empresa) {
-        if (empresa != null) {
-            this.empresa = empresa;
-        }
-    }
-
-    public Container() {
-    }
+    public Container() {}
 
     public Container(float peso) {
         setPeso(peso);
     }
-
 }
